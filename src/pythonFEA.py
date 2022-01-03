f2fc1a47afc9a5610d42e494a58133f8e016a0ba
honzatomek <jan.tomek@protonmail.com>
2022-01-03 19:23:17 +0100

    properties, materials, structure

--- src/pythonFEA.py
+++ src/pythonFEA.py
@@ -25,6 +25,31 @@ class WrongType(BaseError):
 class NotInitialised(BaseError):
   pass
 
+class NotImplemented(BaseError):
+  pass
+
+class NoEntry(BaseError):
+  pass
+
+class NodeMissing(BaseError):
+  pass
+
+class ElementMissing(BaseError):
+  pass
+
+class LoadMissing(BaseError):
+  pass
+
+class PropertyMissing(BaseError):
+  pass
+
+class MaterialMissing(BaseError):
+  pass
+
+class SituationMissing(BaseError):
+  pass
+
+
 class Node:
   def __init__(self, id, x, y, z, label = None):
     self.__id = id
@@ -119,8 +144,8 @@ class Beam2D:
   def __init__(self, id, pid, mid, nid1, nid2, rx1 = False, rz1 = False, rfi1 = False, rx2 = False, rz2 = False, rfi2 = False, label = None, mi = 0.5):
     self.__id = id
     self.__n1 = n1
-    self.__n2 = n2
     self.__r1 = [rx1, rz1, rfi1]
+    self.__n2 = n2
     self.__r2 = [rx2, rz2, rfi2]
     self.__p = pid
     self.__m = mid
@@ -302,6 +327,9 @@ class Beam2D:
 
   @property
   def t(self):
+    '''
+    transformation matrix GCS -> LCS
+    '''
     c = (self.n2.x - self.n1.x) / self.l
     s = (self.n2.y - self.n1.y) / self.l
 
@@ -516,23 +544,664 @@ class Elements:
     return self.__count
 
   def init(self, nodes, props, mats):
-    for i in self.__items:
-      i.init(nodes, props, mats):
+    for id, el in self.__items.items():
+      el.init(nodes, props, mats):
     self.__initialised = True
 
+  @property
+  def initialised(self):
+    if not self.__initialised:
+      return False
+    for id, el in self.__items.items():
+      if not el.initialised:
+        self.__initialised = False
+        return False
+    self.__initialised = True
+    return True
 
-class Constraint:
-  def __init__(self, id, ndid, tx = 0.0, ty = 0.0, tz = 0.0, rx = 0.0, ry = 0.0, rz = 0.0, label = None):
+
+class ConstraintNodal:
+  def __init__(self, id, cset, ndid, t1 = 0.0, t2 = 0.0, t3 = 0.0, r1 = 0.0, r2 = 0.0, r3 = 0.0, label = None):
     self.__id = id
+    self.__cset = cset
     self.__nd = ndid
-    self.__prescribed = list(tx, ty, tz, rx, ry, rz)
+    self.__prescribed = np.asarray([tx, ty, tz, rx, ry, rz], dtype=float)
+    self.__label = label
+
+    self.__loc = None
+
+    self.__initialised = False
+
+  def init(self, nodes):
+    n = nodes.id(self.__nd)
+    if n:
+      self.__nd = n
+    else:
+      raise MissingIndex(f'Node ID {self.__nd} is missing.')
+    self.__initialised = True
+
+  @property
+  def id(self):
+    return self.__id
+
+  @id.setter
+  def id(self, id):
+    self.__id = id
+
+  @property
+  def cset(self):
+    return self.__cset
+
+  @cset.settet
+  def cset(self, cset):
+    self.__cset = cset
+
+  @property
+  def nd(self):
+    return self.__nd
+
+  @nd.setter
+  def nd(self, node):
+    self.__nd = node
+    if type(nd) is Node:
+      self.__initialised = True
+    else:
+      self.__initialised = False
+
+  @property
+  def label(self):
+    return self.__label
+
+  @label.setter
+  def label(self, label):
+    self.__label = label
+
+  @property
+  def prescribe(self):
+    return self.__prescribed
+
+  @prescribe.setter
+  def prescribe(self, t1 = 0.0, t2 = 0.0, t3 = 0.0, r1 = 0.0, r2 = 0.0, r3 = 0.0)
+    self.__prescribed = np.asarray([t1, t2, t3, r1, r2, r3], dtype=float)
+
+  @property
+  def initialised(self):
+    return self.__initialised
 
 
+class Constraints:
+  def __add(self, constraint):
+    if constraint is None:
+      pass
+    if type(constraint) is not ConstraintNodal:
+      raise WrongType(f'Constraint is not of type ConstraintNodal, but {type(constraint)} - {str(constraint)}.')
+    if constraint.id not in self.__items:
+      raise UsedIndex(f'Constraint ID {constraint.id} already exists.')
+    self.__items.setdefault(constraint.id, constraint)
+    self.__count += 1
+
+  def __init__(self):
+    self.__count = 0
+    self.__items = dict()
+
+    self.__initialised = False
+
+  def id(self, id):
+    if id in self.__items:
+      return self.__items[id]
+    else:
+      return False
+
+  def add(self, constraint = None):
+    self.__add(constraint)
+
+  @property
+  def count(self):
+    return self.__count
+
+  def init(self, nodes):
+    for id, c in self.__items.items():
+      c.init(nodes):
+    self.__initialised = True
+
+  @property
+  def initialised(self):
+    if not self.__initialised:
+      return False
+    for id, c in self.__items.items()
+      if not c.initialised:
+        self.__initialised = False
+        return False
+    self.__initialised = True
+    return True
 
 
+class LoadNodal:
+  type = 'node'
+
+  def __init__(self, id, lpat, ndid, F1 = 0.0, F2 = 0.0, F3 = 0.0, M1 = 0.0, M2 = 0.0, M3 = 0.0, label = None):
+    self.__id = id
+    self.__lpat = lpat
+    self.__nd = ndid
+    self.__forces = np.asarray([F1, F2, F3, M1, M2, M3], dtype=float)
+
+    self.__initialised = False
+
+  def init(self, nodes):
+    n = nodes.id(self.__nd)
+    if n:
+      self.__nd = n
+    else:
+      raise MissingIndex(f'Node ID {self.__nd} is missing.')
+    self.__initialised = True
+
+  @property
+  def load_type(self):
+    return self.type
+
+  @property
+  def id(self):
+    return self.__id
+
+  @id.setter
+  def id(self, id):
+    self.__id = id
+
+  @property
+  def lpat(self):
+    return self.__lpat
+
+  @lpat.setter
+  def lpat(self, lpat):
+    self.__lpat = lpat
+
+  @property
+  def nd(self):
+    return self.__nd
+
+  @nd.setter
+  def nd(self, node):
+    self.__nd = node
+    if type(nd) is Node:
+      self.__initialised = True
+    else:
+      self.__initialised = False
+
+  @property
+  def label(self):
+    return self.__label
+
+  @label.setter
+  def label(self, label):
+    self.__label = label
+
+  @property
+  def F1(self):
+    return self.__forces[0]
+
+  @F1.setter
+  def F1(self, F1):
+    self.__forces[0] = F1
+
+  @property
+  def F2(self):
+    return self.__forces[1]
+
+  @F2.setter
+  def F2(self, F2):
+    self.__forces[1] = F2
+
+  @property
+  def F3(self):
+    return self.__forces[2]
+
+  @F3.setter
+  def F3(self, F3):
+    self.__forces[2] = F3
+
+  @property
+  def M1(self):
+    return self.__forces[3]
+
+  @M1.setter
+  def M1(self, M1):
+    self.__forces[3] = M1
+
+  @property
+  def M2(self):
+    return self.__forces[4]
+
+  @M2.setter
+  def M2(self, M2):
+    self.__forces[4] = M2
+
+  @property
+  def M3(self):
+    return self.__forces[5]
+
+  @M3.setter
+  def M3(self, M3):
+    self.__forces[5] = M3
+
+  @propery
+  def forces(self):
+    return self.__forces
+
+  @forces.setter
+  def forces(self, F1 = 0.0, F2 = 0.0, F3 = 0.0, M1 = 0.0, M2 = 0.0, M3 = 0.0)
+    self.__forces = np.asarray([F1, F2, F3, M1, M2, M3], dtype=float)
 
 
+class LoadBeam2D:
+  type = 'element'
 
+  self.__2gcs(self):
+    if self.__dir == 'gcs':
+      pass
+    else:
+      self.__forces = self.t @ self.__forces
+      self.__dir = 'gcs'
+
+  self.__2lcs(self):
+    if self.__dir = 'element':
+      pass
+    else:
+      self.__forces = self.t.T @ self.__forces
+      self.__dir = 'element'
+
+  self.__init__(self, id, lpat, eid, fx = 0.0, fz = 0.0, dir = 'element', label = None):
+    '''
+    dir = element/gcs
+    '''
+    self.__id = id
+    self.__lpat = lpat
+    self.__el = eid
+    self.__dir = dir
+    self.__forces = np.asarray([[fx], [fz]], dtype=float)
+
+    self.__label = label
+
+    self.__initialised = False
+
+  def init(self, elements):
+    e = elements.id(self.__el)
+    if e:
+      self.el = e
+    else:
+      raise MissingIndex(f'Element ID {self.__el} is missing.')
+    self.__2lcs()
+    self.__initialised = True
+
+  @property
+  def load_type(self):
+    return self.type
+
+  @property
+  def id(self):
+    return self.__id
+
+  @id.setter
+  def id(self, id):
+    self.__id = id
+
+  @property
+  def lpat(self):
+    return self.__lpat
+
+  @lpat.setter
+  def lpat(self, lpat):
+    self.__lpat = lpat
+
+  @property
+  def el(self):
+    return self.__el
+
+  @el.setter
+  def el(self, element):
+    if type(element) is not int or type(element) is not Beam2D:
+      raise WrongType(f'Element Type for Load ID {self.__id} is not Beam2D, but {type(element)}.')
+    self.__el = element
+    if type(element) is Beam2D:
+      self.__initialised = True
+    else:
+      self.__initialised = False
+
+  @property
+  def label(self):
+    return self.__label
+
+  @label.setter
+  def label(self, label):
+    self.__label = label
+
+  @property
+  def fx(self):
+    return self.__forces[0]
+
+  @fx.setter
+  def fx(self, fx):
+    self.__forces[0] = fx
+
+  @property
+  def fz(self):
+    return self.__forces[1]
+
+  @fz.setter
+  def fz(self, fz):
+    self.__forces[1] = fz
+
+  @property
+  def forces(self):
+    return self.__forces
+
+  @forces.setter
+  def forces(self, fx = 0.0, fz = 0.0, dir = 'element'):
+    '''
+    dir = element/gcs
+    '''
+    self.__dir = dir
+    self.__forces = np.asarray([[fx], [fz]], dtype=float)
+
+  @property
+  def t(self):
+    if self.__initialised:
+      # transformation GCS -> LCS
+      t = self.__el.t
+
+      return t[0:2, 0:2]
+
+
+class Loads:
+  def __add(self, load):
+    if load is None:
+      pass
+    if type(load) is not LoadNodal type(load) is not LoadBeam2D:
+      raise WrongType(f'Load is not of type Load, but {type(load)} - {str(load)}.')
+    if load.id not in self.__items:
+      raise UsedIndex(f'Load ID {load.id} already exists.')
+    self.__items.setdefault(load.id, load)
+    self.__lpat.setdefault(load.lpat, [])
+    self.__lpat[load.lpat].append(load)
+    self.__count += 1
+
+  def __init__(self):
+    self.__count = 0
+    self.__items = dict()
+    self.__lpat = dict()
+
+    self.__initialised = False
+
+  def id(self, id):
+    if id in self.__items:
+      return self.__items[id]
+    else:
+      return False
+
+  def lpats(self):
+    return list(self.__lpat.keys())
+
+  def lpat(self, lpatid):
+    # lpatloads = list()
+    # for id, l in self.__items.items():
+    #   if l.lpat == lpatid:
+    #     lpatloads.append(l)
+    # return lpatloads
+    return list(self.__lpat.values())
+
+
+  def add(self, load = None):
+    self.__add(load)
+
+  @property
+  def count(self):
+    return self.__count
+
+  def init(self, nodes, elements):
+    for id, l in self.__items.items():
+      if l.type = 'node':
+        l.init(nodes)
+      elif l.type = 'element':
+        l.init(elements)
+    return self.initialised
+
+  @property
+  def initialised(self):
+    if not self.__initialised:
+      return False
+    for id, l in self.__items.items()
+      if not l.initialised:
+        self.__initialised = False
+        return False
+    self.__initialised = True
+    return True
+
+
+class PropertyBeam2D:
+  def __init__(self, id, A, I, nsm = 0.0, label = None):
+    self.__id = id
+    self.__A = A
+    self.__I = I
+    self.__nsm = nsm
+    self.__label = label
+
+  @property
+  def id(self):
+    return self.__id
+
+  @id.setter
+  def id(self, id):
+    self.__id = id
+
+  @property
+  def A(self):
+    return self.__A
+
+  @A.setter
+  def A(self, A):
+    self.__A = A
+
+  @property
+  def I(self):
+    return self.__I
+
+  @I.setter
+  def I(self, I):
+    self.__I = I
+
+  @property
+  def nsm(self):
+    return self.__nsm
+
+  @nsm.setter
+  def nsm(self, nsm):
+    self.__nsm = nsm
+
+  @property
+  def label(self):
+    return self.__label
+
+  @label.setter
+  def label(self, label):
+    self.__label = label
+
+
+class Properties:
+  def __add(self, prop):
+    if prop is None:
+      pass
+    if type(prop) is not PropertyBeam2D:
+      raise WrongType(f'Property is not of type PropertyBeam2D, but {type(prop)} - {str(prop)}.')
+    if prop.id not in self.__items:
+      raise UsedIndex(f'Property ID {prop.id} already exists.')
+    self.__items.setdefault(prop.id, prop)
+    self.__count += 1
+
+  def __init__(self):
+    self.__count = 0
+    self.__items = dict()
+
+    self.__initialised = False
+
+  def id(self, id):
+    if id in self.__items:
+      return self.__items[id]
+    else:
+      return False
+
+  def add(self, prop = None):
+    self.__add(prop)
+
+
+def MaterialLinear2D:
+  def __init__(self, id, E, ro, alpha, label = None):
+    self.__id = id
+    self.__E = E
+    self.__ro = ro
+    self.__alpha = alpha
+    self.__label = label
+
+  @property
+  def id(self):
+    return self.__id
+
+  @id.setter
+  def id(self, id):
+    self.__id = id
+
+  @propery
+  def E(self):
+    return self.__E
+
+  @E.setter
+  def E(self, E):
+    self.__E = E
+
+  @propery
+  def ro(self):
+    return self.__ro
+
+  @ro.setter
+  def ro(self, ro):
+    self.__ro = ro
+
+  @propery
+  def alpha(self):
+    return self.__alpha
+
+  @alpha.setter
+  def alpha(self, alpha):
+    self.__alpha = alpha
+
+  @propery
+  def label(self):
+    return self.__label
+
+  @label.setter
+  def label(self, label):
+    self.__label = label
+
+
+class Materials:
+  def __add(self, mat):
+    if mat is None:
+      pass
+    if type(mat) is not MaterialLinear2D:
+      raise WrongType(f'Material is not of type MaterialLinear2D, but {type(mat)} - {str(mat)}.')
+    if mat.id not in self.__items:
+      raise UsedIndex(f'Material ID {mat.id} already exists.')
+    self.__items.setdefault(mat.id, mat)
+    self.__count += 1
+
+  def __init__(self):
+    self.__count = 0
+    self.__items = dict()
+
+    self.__initialised = False
+
+  def id(self, id):
+    if id in self.__items:
+      return self.__items[id]
+    else:
+      return False
+
+  def add(self, mat = None):
+    self.__add(mat)
+
+
+class Structure:
+  def __init__(self, label):
+    self.__label = label
+    self.__structure = dict()
+    self.__nodes = Nodes()
+    self.__elements = Elements()
+
+  def new(self, situation, constraints = None, loads = None, props = None, mats = None):
+    self.__structure.setdefault(situation, dict())
+    self.__structure[situation]['constraints'] = constraints if constraints else Constraints()
+    self.__structure[situation]['loads'] = loads if loads else Loads()
+    self.__structure[situation]['properties'] = props if props else Properties()
+    self.__structure[situation]['materials'] = mats if mats else Materials()
+
+  @propery
+  def label(self):
+    return self.__label
+
+  @label.setter
+  def label(self, label):
+    self.__label = label
+
+  @property
+  def nodes(self):
+    return self.__nodes
+
+  @property
+  def elements(self):
+    return self.__elements
+
+  def constraints(self, situation):
+    return self.__structure[situation]['constraints']
+
+  def loads(self, situation):
+    return self.__structure[situation]['loads']
+
+  def properties(self, situation):
+    return self.__structure[situation]['properties']
+
+  def materials(self, situation):
+    return self.__structure[situation]['materials']
+
+  def init(self, situation = None):
+    if situation and situation not in self.__structure.keys():
+      raise SituationMissing(f'Structure {self.label} does not contain Situation {situation}.')
+    sit = [situation] if situation else list(self.__structure.keys())
+
+    for s in sit:
+      self.elements.init(self.__nodes, self.properties(s), self.materials(s))
+      self.constraints(s).init(self.__nodes)
+      self.loads(s).init(self._nodes, self.__elements)
+
+  @property
+  def check(self):
+    # check nodes
+    if self.nodes.count == 0:
+      raise NodeMissing(f'Structure {self.__label} does not contain any Nodes.')
+
+    # check elements
+    if self.elements.count == 0:
+      raise ElementMissing(f'Structure {self.__label} does not contain any Elements.')
+
+    for sit in self.__structure.keys():
+      # check loads
+      if self.__structure[sit]['loads'].count == 0:
+        raise LoadMissing(f'Structure {self.__label} does not contain any Loads in Situation {sit}.')
+
+      # check properties
+      if self.__structure[sit]['properties'].count == 0:
+        raise PropertyMissing(f'Structure {self.__label} does not contain any Properties in Situation {sit}.')
+
+      # check materials
+      if self.__structure[sit]['materials'].count == 0:
+        raise PropertyMissing(f'Structure {self.__label} does not contain any Materials in Situation {sit}.')
 
 
 
